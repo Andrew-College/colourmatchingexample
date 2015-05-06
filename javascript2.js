6eb37f8eb052b35740a126970b80a0bb65de5c87
AndrewWindows <andrewsansomwork@gmail.com>
2015-05-06 13:42:57 +0100

    removable components

--- javascript2.js
+++ javascript2.js
@@ -1,19 +1,16 @@
 var canvas = document.getElementById("mainCanvas");
 var c = canvas.getContext("2d");
+c.imageSmoothingEnabled = false;
 
 var path = "http://wonderfl.net/images/icon/e/ec/ec3c/ec3c37ba9594a7b47f1126b2561efd35df2251bfm";
 var stop = true;
 
 
 
+$(document)
+.ready(checkerBackground())
+.resize(checkerBackground());
 
-
-
-
-
-
-
-$(document).resize(checkerBackground());
 
 function Element(path){
   var that = this;
@@ -28,36 +25,42 @@ function webpage(json){
     // options...
   });
 }
-
-function addText(){
+function addMisc(){
 	var text = webpage({
 		  "text" :
-		  "<div id='text' class='first'><div></div><div><div class='edge'>files<div>hello</div></div><div class='center'>files<div>hello</div></div><div class='edge'>files<div>hello</div></div></div></div>"
+		  "<div id='text' class='first'><div>text</div><div><div class='edge'>files<div>hello</div></div><div class='center'>files<div>hello</div></div><div class='edge'>files<div>hello</div></div></div></div>"
 		});
+}
+
+function addText(){
+
+	$('#logging').append($.parseHTML("<div id='text' class='first container'><div><div style='display: inline-block;'>text</div><div style='display: inline-block;float: right; cursor: pointer;' onclick='deleteThis(this)'>X</div></div><div><div class='edge'><div>o</div></div><div class='center'><div style='padding-left: 5%;padding-right: 5%;'><input type='text'style='width: 100%;'></div></div><div class='edge'><div>o</div></div></div></div>"));
+	  dragger();
 }
 
 function addPath(){
-	var path = webpage({
-		  "text" :
-		  "<div id='path' class='first'><div></div><div><div class='edge'>files<div>goodbye</div></div><div class='center'>files<div>hello</div></div><div class='edge'>files<div>hello</div></div></div></div>"
-		});
+	$('#logging').append($.parseHTML("<div id='path' class='first container'><div><div style='display: inline-block;'>path</div><div style='display: inline-block;float: right; cursor: pointer;' onclick='deleteThis(this)'>X</div></div><div><div class='edge'><div>o</div></div><div class='center'><div style='padding-left: 5%;padding-right: 5%;'><input type='text'style='width: 100%;'></div></div><div class='edge'><div>o</div></div></div></div>"));
+	  dragger();
 }
 
 function addMath(){
-	var math = webpage({
-		  "text" :
-		  "<div id='math' class='first'><div></div><div><div class='edge'>files<div>goodbye</div></div><div class='center'><div><select><option value='Add'>Add</option><option value='Subtract'>Subtract</option><option value='Multiply'>Multiply</option><option value='Divide'>Divide</option><option value='Modulo'>Modulo</option><option value='Exponent'>Exponent</option></select></div></div><div class='edge'>files<div>hello</div></div></div></div>"
-		});
+    $('#logging').append($.parseHTML("<div id='math' class='first container'><div><div style='display: inline-block;'>math</div><div style='display: inline-block;float: right; cursor: pointer;' onclick='deleteThis(this)'>X</div></div><div><div class='edge'><div>o</div><div>o</div></div><div class='center'><div><select><option value='Add'>Add</option><option value='Subtract'>Subtract</option><option value='Multiply'>Multiply</option><option value='Divide'>Divide</option><option value='Modulo'>Modulo</option><option value='Exponent'>Exponent</option></select></div></div><div class='edge'><div>o</div></div></div></div>"));
+  	  dragger();
 }
 
 function addShader(){
-	var shader = webpage({
-		  "text" :
-		  "<div id='shader' class='first'><div></div><div><div class='edge'>files<div>goodbye</div></div><div class='center'>files<div>hello</div></div><div class='edge'>files<div>hello</div></div></div></div>"
-		});
+  $('#logging').append($.parseHTML("<div id='shader' class='first container'><div><div style='display: inline-block;'>shader</div><div style='display: inline-block;float: right; cursor: pointer;' onclick='deleteThis(this)'>X</div></div><div><div class='edge'><div>o</div></div><div class='center'><div style='padding-left: 5%;padding-right: 5%;'><input type='text'style='width: 100%;'></div></div><div class='edge'><div>o</div></div></div></div>"));
+	  dragger();
+}
+
+function dragger(){
+	$('.first').draggabilly({
+	    // options...
+	  });
 }
 
 function checkerBackground(){
+	console.log("checker");
   var blockSize = 10;
   var flipflop = true;
   for(var i = 0; i < $('#mainCanvas').width(); i += blockSize){
@@ -69,3 +72,13 @@ function checkerBackground(){
     flipflop = !flipflop;
   }
 }
+
+var test;
+
+function deleteThis(object){
+  $(object).parent().parent().remove();
+}
+
+$(window).bind('resize', function () {
+    checkerBackground();
+}).trigger('resize');
